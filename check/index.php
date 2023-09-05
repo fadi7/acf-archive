ffed73c3e7332b6ae33db1970a586b90b27a4ff6
fadi7 <mfk17@live.com>
2023-09-05 12:19:44 +0400

    add chek index empty

---
+++ check/index.php
@@ -0,0 +1,11 @@
+<?php
+// require  '../vendor/autoload.php';
+// require_once("../prapare.php");
+// include("../functions.php");
+
+// include("../links.php");
+
+// include("main.php");
+// print_out();
+
+echo "Coming Soon";
